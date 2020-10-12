f1f10cbb462c5e7920c367c99818f2df77e39b4e
lijingchun0 <359120730@qq.com>
2020-10-12 16:45:27 +0800

    Update elrepo.sh

--- shell/elrepo.sh
+++ shell/elrepo.sh
@@ -3,7 +3,7 @@
 set -e
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+yum install -y https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 echo y | yum --enablerepo=elrepo-kernel install kernel-lt
 sed -i 's/saved/0/1g' /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
